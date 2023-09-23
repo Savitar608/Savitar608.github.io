1578e3e2929d0eb7fc1cbb4fe68d88a9a4b158d9
Adithya <adithya608@gmail.com>
2023-09-23 21:56:34 +0000

    Some improvements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // src/App.js
 
-import React, { Component }  from 'react';
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
+import React, { Component }  from 'react'
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Testimonials from "./components/Testimonials"
+
+require("dotenv").config()
 
 export default function App() {
   return (
@@ -18,5 +20,5 @@ export default function App() {
       <Testimonials />
       <Contact />
     </main>
-  );
+  )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,138 +1,168 @@
 // src/components/Contact.js
 
-import React from "react";
-import emailjs from 'emailjs-com';
+import React, { useRef } from 'react'
+// import emailjs from 'emailjs-com'
+import ReCAPTCHA from "react-google-recaptcha"
+
+require("dotenv").config()
+
+const { default: axios } = require("axios")
 
 export default function Contact() {
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [subject, setSubject] = React.useState("");
-    const [message, setMessage] = React.useState("");
+	const [name, setName] = React.useState("")
+	const [email, setEmail] = React.useState("")
+	const [subject, setSubject] = React.useState("")
+	const [message, setMessage] = React.useState("")
+
+	const captchaRef = useRef(null)
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+
+		const token = captchaRef.current.getValue()
+		captchaRef.current.reset()
+
+		await axios.post("http://localhost:2000/post", { token })
+			.then(res => {
+				if (res.data === true)
+					sendEmail(token)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
 
 
-    // function onClick(e) {
-    //     e.preventDefault();
-    //     grecaptcha.ready(function () {
-    //         grecaptcha.execute('reCAPTCHA_site_key', { action: 'submit' }).then(function (token) {
-    //             // Add your logic to submit to your backend server here.
-    //         });
-    //     });
-    // }
+	const sendEmail = async (token) => {
+		let params = {
+			service_id: process.env.REACT_APP_EMAIL_SERVICE_ID,
+			template_id: process.env.REACT_APP_EMAIL_TEMPLATE_ID,
+			user_id: process.env.REACT_APP_EMAIL_KEY,
+			template_params: {
+				"name": document.getElementById("name").value,
+				"email": document.getElementById("email").value,
+				"subject": document.getElementById("subject").value,
+				"message": document.getElementById("message").value,
+				"g-recaptcha-response": token
+			}
+		}
 
-    function sendEmail(e) {
-        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+		console.log(params)
 
-        // need some way to hide the key
-        emailjs.sendForm('service_jjw4t5h', 'template_0q91nvd', e.target, 'CO45DM9qTa8isu7S9')
-            .then(() => {
-                window.location.reload()  // If the page should be reloaded after sending email
-            }, (error) => {
-                console.log(error.text);
-            });
-    }
+		await axios.post("https://api.emailjs.com/api/v1.0/email/send", params)
+			.then(() => {
+				window.location.reload()  // If the page should be reloaded after sending email
+			}, (error) => {
+				console.log(error.text)
+			})
+	}
 
-    return (
-        <section id="contact" className="relative">
-            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                    <iframe
-                        width="100%"
-                        height="100%"
-                        title="map"
-                        className="absolute inset-0"
-                        style={{ filter: "opacity(0.7)" }}
-                        src="https://www.google.com/maps/embed/v1/place?q=Navya+Elite+Apartmnets,+27th+Main+Road,+VGS+Layout,+Muneshwara+Swamy+Layout,+Ejipura,+Bengaluru,+Karnataka,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-                    />
-                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-                        <div className="lg:w-1/2 px-6">
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                                ADDRESS
-                            </h2>
-                            <p className="mt-1">
-                                Navya Elite Apartments, Ejpura <br />
-                                Bangalore, Karnataka 560047
-                            </p>
-                        </div>
-                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                                EMAIL
-                            </h2>
-                            <a className="text-indigo-400 leading-relaxed">
-                                adithya608@gmail.com
-                            </a>
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                                PHONE
-                            </h2>
-                            <p className="leading-relaxed">+91-8848309497</p>
-                        </div>
-                    </div>
-                </div>
-                <form
-                    name="contact-form"
-                    onSubmit={sendEmail}
-                    className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                        Contact me
-                    </h2>
-                    <p className="leading-relaxed mb-5">
-                        Hit me up if you want to discuss any collaborative work opportunities or even for just a normal conversation.
-                    </p>
-                    <div className="relative mb-4">
-                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="subject" className="leading-7 text-sm text-gray-400">
-                            Subject
-                        </label>
-                        <input
-                            type="subject"
-                            id="subject"
-                            name="subject"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setSubject(e.target.value)}
-                        />
-                    </div>
-                    <div className="relative mb-4">
-                        <label
-                            htmlFor="message"
-                            className="leading-7 text-sm text-gray-400">
-                            Message
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setMessage(e.target.value)}
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                        Submit
-                    </button>
-                </form>
-            </div>
-        </section>
-    );
+	return (
+		<section id="contact" className="relative">
+			<div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+				<div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+					<iframe
+						width="100%"
+						height="100%"
+						title="map"
+						className="absolute inset-0"
+						style={{ filter: "opacity(0.7)" }}
+						src="https://www.google.com/maps/embed/v1/place?q=Navya+Elite+Apartmnets,+27th+Main+Road,+VGS+Layout,+Muneshwara+Swamy+Layout,+Ejipura,+Bengaluru,+Karnataka,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+					/>
+					<div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+						<div className="lg:w-1/2 px-6">
+							<h2 className="title-font font-semibold text-white tracking-widest text-xs">
+								ADDRESS
+							</h2>
+							<p className="mt-1">
+								Navya Elite Apartments, Ejpura <br />
+								Bangalore, Karnataka 560047
+							</p>
+						</div>
+						<div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+							<h2 className="title-font font-semibold text-white tracking-widest text-xs">
+								EMAIL
+							</h2>
+							<a className="text-indigo-400 leading-relaxed">
+								adithya608@gmail.com
+							</a>
+							<h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+								PHONE
+							</h2>
+							<p className="leading-relaxed">+91-8848309497</p>
+						</div>
+					</div>
+				</div>
+				<form
+					name="contact-form"
+					onSubmit={handleSubmit}
+					className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+					<h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+						Contact me
+					</h2>
+					<p className="leading-relaxed mb-5">
+						Hit me up if you want to discuss any collaborative work opportunities or even for just a normal conversation.
+					</p>
+					<div className="relative mb-4">
+						<label htmlFor="name" className="leading-7 text-sm text-gray-400">
+							Name
+						</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div className="relative mb-4">
+						<label htmlFor="email" className="leading-7 text-sm text-gray-400">
+							Email
+						</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div className="relative mb-4">
+						<label htmlFor="subject" className="leading-7 text-sm text-gray-400">
+							Subject
+						</label>
+						<input
+							type="subject"
+							id="subject"
+							name="subject"
+							className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+							onChange={(e) => setSubject(e.target.value)}
+						/>
+					</div>
+					<div className="relative mb-4">
+						<label
+							htmlFor="message"
+							className="leading-7 text-sm text-gray-400">
+							Message
+						</label>
+						<textarea
+							id="message"
+							name="message"
+							className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+					</div>
+					<ReCAPTCHA
+						sitekey={process.env.REACT_APP_SITE_KEY}
+						ref={captchaRef}
+					/>
+					<button
+						type="submit"
+						className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+						Submit
+					</button>
+				</form>
+			</div>
+		</section>
+	)
 }
